fe838c70b48de423173d5fc030678404eb02ab56
Paulo Rikardo <minifznn@gmail.com>
2025-05-07 14:13:12 -0300

    feat: New system validate

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,16 +1,16 @@
 import express from 'express'
 import { createUser, getAllUsers, deleteUser, updateUser, getUserId } from '../controllers/userController.js'
-
-
+import { validate } from '../middleware/validate.js'
+import { createUserSchema, updatedUserSchema } from '../schemas/userSchemas.js'
 const router = express.Router()
 
 router.get('/', getAllUsers)
 
 router.get("/:id", getUserId)
 
-router.post('/', createUser)
+router.post('/', validate(createUserSchema), createUser)
 
-router.put('/:id', updateUser)
+router.put('/:id', updateUser(updatedUserSchema), updateUser)
 
 router.delete('/:id', deleteUser)
 
